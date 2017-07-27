93e19bc1fb9bcb3636cbe8eb55c7048ee1d999db
hiluluke <hiluluke@hotmail.com>
2017-07-27 23:43:11 +0800

    add first model

--- function_tests.py
+++ function_tests.py
@@ -8,6 +8,10 @@ class NewVistorTest(unittest.TestCase):
         self.browser.implicitly_wait(3)
     def tearDown(self):
         self.browser.quit()
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
     def test_can_start_it_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do', self.browser.title)
@@ -20,14 +24,15 @@ class NewVistorTest(unittest.TestCase):
         )
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
         table = self.browser.find_element_by_id('id_list_table')
-        
+        import time
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
-        )
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
-        self.fail('Finish the Test');
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
--- lists/tests.py
+++ lists/tests.py
@@ -3,8 +3,14 @@ from django.template.loader import render_to_string
 from django.test import TestCase
 from django.http import HttpRequest
 from lists.views import home_page
+from lists.models import Item
+import re
 
 class HomePageTest(TestCase):
+    def remove_csrf(self, html_code):
+        csrf_regex = r'<input[^>]+csrfmiddlewaretoken[^>]+>'
+        return re.sub(csrf_regex, '', html_code)
+
     def test_root_url_resolvers_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
@@ -12,4 +18,29 @@ class HomePageTest(TestCase):
         request = HttpRequest()
         response = home_page(request)
         expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html)
+        self.assertEqual(self.remove_csrf(response.content.decode()), expected_html)
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+        response = home_page(request)
+        self.assertIn('A new list item', response.content.decode())
+        expected_html = render_to_string(
+            'home.html',
+            {'new_item_text':  'A new list item'}
+        )
+        self.assertEqual(self.remove_csrf(response.content.decode()), expected_html)
+class ItemTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
