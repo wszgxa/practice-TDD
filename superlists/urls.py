76cff4ebf69f452c42cf351334c91a7f586fdaff
hiluluke <hiluluke@hotmail.com>
2017-07-29 10:46:49 +0800

    udpate

--- superlists/urls.py
+++ superlists/urls.py
@@ -18,7 +18,8 @@ from lists import views
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$', views.home_page, name='home'),
-    url(r'^lists/the-only-list-in-the-world/$', views.view_list, name='view_list')
+    url(r'^$', views.home_page, name = 'home'),
+    url(r'^lists/the-only-list-in-the-world/$', views.view_list, name = 'view_list'),
+    url(r'^lists/new$', views.new_list, name = 'new_item')
     # url(r'^admin/', admin.site.urls),
 ]
